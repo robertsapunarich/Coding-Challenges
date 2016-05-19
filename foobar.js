aa3eac448b2067dca80a8019d9ce4a3e16dcb312
robertsapunarich <robertsapunarich@gmail.com>
2016-05-19 17:06:20 +0000

    Implement foobar logic with a `for` loop and nested conditional. Remove debugging code.

--- foobar.js
+++ foobar.js
@@ -8,9 +8,18 @@ var readlineSync = require('readline-sync');
 // Get user input and convert it to an integer
 var input = parseInt(readlineSync.question("How many foos would you like to bar? "));
 
+/* Foobar logic. If an integer is a multiple of 3, print "foo" to the console.
+If it's a multiple of 5, print "bar". If it's a multiple of both, print "foobar".
+In all other cases, print the integer. */
 
-// Foobar logic
-
-// Log to console for debugging purposes.
-console.log(input);
-console.log(typeof(input));
+for (var iteration = 1; iteration <= input; iteration++) {
+  if (iteration % 3 === 0 && iteration % 5 === 0) {
+    console.log("foobar");
+  } else if (iteration % 3 === 0) {
+    console.log("foo");
+  } else if (iteration % 5 === 0) {
+    console.log("bar");
+  } else {
+    console.log(iteration);
+  }
+}
