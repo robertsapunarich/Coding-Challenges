f87efd098f3738d94aa8ba04acabb09b61688cf6
robertsapunarich <robertsapunarich@gmail.com>
2016-05-19 16:55:14 +0000

    Begin work on foobar problem in JavaScript.

---
+++ foobar.js
@@ -0,0 +1,16 @@
+// The foobar problem (aka fizzbuzz) implemented in JavaScript.
+// Still in progress.
+
+
+// Require `readline-sync` to get user input
+var readlineSync = require('readline-sync');
+
+// Get user input and convert it to an integer
+var input = parseInt(readlineSync.question("How many foos would you like to bar? "));
+
+
+// Foobar logic
+
+// Log to console for debugging purposes.
+console.log(input);
+console.log(typeof(input));
